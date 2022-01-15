c8ceec0c4e16392f6efbd74686df3dd1ba542383
Jaewon <turitus@naver.com>
2022-01-16 01:55:46 +0900

    [UPDATE] my gomins viewmodel

--- GOMIN_ANDROID/app/src/main/java/com/example/gomin/viewmodel/MyGominsViewModel.kt
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/viewmodel/MyGominsViewModel.kt
@@ -1,6 +1,25 @@
 package com.example.gomin.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.gomin.adapter.RecyclerViewItem
+import com.example.gomin.repository.MyGominRepository
 
-class MyGominsViewModel: ViewModel() {
+class MyGominsViewModel(private val myGominRepository: MyGominRepository): ViewModel() {
+
+    private val _myGominItemViews = MutableLiveData<List<RecyclerViewItem>>()
+    val myGominItemViews: LiveData<List<RecyclerViewItem>> = _myGominItemViews
+
+    fun getMyGomins() {
+        myGominRepository.getMyGomins().subscribe { response->
+            if (response.isSuccessful) {
+                _myGominItemViews.value = response.body()!!.map {
+                    RecyclerViewItem(
+                        
+                    )
+                }
+            }
+        }
+    }
 }
