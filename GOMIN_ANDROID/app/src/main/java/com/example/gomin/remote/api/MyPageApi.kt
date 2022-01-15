63a98ea48a89b0795241bc67073ce403e5de3385
Jaewon <turitus@naver.com>
2022-01-16 01:53:28 +0900

    [CREATE] my gomin repository

--- GOMIN_ANDROID/app/src/main/java/com/example/gomin/remote/api/MyPageApi.kt
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/remote/api/MyPageApi.kt
@@ -1,5 +1,6 @@
 package com.example.gomin.remote.api
 
+import com.example.gomin.model.MyGominEntity
 import com.example.gomin.model.MyInfoEntity
 import io.reactivex.rxjava3.core.Single
 import retrofit2.Response
@@ -8,4 +9,7 @@ import retrofit2.http.GET
 interface MyPageApi {
     @GET("member/info")
     fun getUserInfo(): Single<Response<MyInfoEntity>>
+
+    @GET("member/my/page")
+    fun getUserGominList(): Single<Response<List<MyGominEntity>>>
 }
