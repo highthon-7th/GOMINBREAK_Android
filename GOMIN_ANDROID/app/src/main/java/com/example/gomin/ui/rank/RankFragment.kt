2ec754c4e564a99d13e7032e5bc0595d65482dea
Jaewon <turitus@naver.com>
2022-01-16 04:17:17 +0900

    [UPDATE] rank viewpager

--- GOMIN_ANDROID/app/src/main/java/com/example/gomin/ui/rank/RankFragment.kt
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/ui/rank/RankFragment.kt
@@ -4,13 +4,25 @@ import com.example.gomin.R
 import com.example.gomin.base.BaseFragment
 import com.example.gomin.databinding.FragmentRankBinding
 import com.example.gomin.viewmodel.RankViewModel
+import com.google.android.material.tabs.TabLayoutMediator
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RankFragment : BaseFragment<FragmentRankBinding>(R.layout.fragment_rank) {
 
     override val vm: RankViewModel by viewModel()
 
-    override fun observeEvent() {
+    companion object {
+        private val titleList = arrayListOf("포토앨범", "공지사항", "가정통신문")
+    }
 
+    override fun observeEvent() {
+        binding.rankVp.run {
+            adapter = RankAdapter(requireActivity())
+            isUserInputEnabled = false
+        }
+        TabLayoutMediator(binding.rankTl, binding.rankVp) { tab, position ->
+            binding.rankVp.currentItem = binding.rankTl.selectedTabPosition
+            tab.text = titleList[position]
+        }.attach()
     }
 }
