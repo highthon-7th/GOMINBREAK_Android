43851c882a6fcdcf2ebedb6d59c88ebb67e5c71e
Jaewon <turitus@naver.com>
2022-01-16 00:03:55 +0900

    [CREATE] mypage api

---
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/remote/api/MyPageApi.kt
@@ -0,0 +1,11 @@
+package com.example.gomin.remote.api
+
+import com.example.gomin.model.MyInfoEntity
+import io.reactivex.rxjava3.core.Single
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface MyPageApi {
+    @GET("member/info")
+    fun getUserInfo(): Single<Response<MyInfoEntity>>
+}
