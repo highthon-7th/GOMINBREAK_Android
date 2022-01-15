cda6600b9d7eba1894a1f0a2685b7a7b1fe415af
Jaewon <turitus@naver.com>
2022-01-16 05:01:17 +0900

    [CREATE] edit activity

---
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/ui/EditInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gomin.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gomin.R
+
+class EditInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_myinfo)
+    }
+}
