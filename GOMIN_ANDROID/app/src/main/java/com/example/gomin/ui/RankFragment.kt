ac8c1ebb65516c835c74b6f921385a8ea24814bf
Jaewon <turitus@naver.com>
2022-01-16 02:48:34 +0900

    [UPDATE] rank viewmodel

--- GOMIN_ANDROID/app/src/main/java/com/example/gomin/ui/RankFragment.kt
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/ui/RankFragment.kt
@@ -1,18 +1,16 @@
 package com.example.gomin.ui
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.gomin.R
+import com.example.gomin.base.BaseFragment
+import com.example.gomin.databinding.FragmentRankBinding
+import com.example.gomin.viewmodel.RankViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class RankFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rank, container, false)
+class RankFragment : BaseFragment<FragmentRankBinding>(R.layout.fragment_rank) {
+
+    override val vm: RankViewModel by viewModel()
+
+    override fun observeEvent() {
+
     }
 }
--- GOMIN_ANDROID/app/src/main/java/com/example/gomin/viewmodel/RankViewModel.kt
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/viewmodel/RankViewModel.kt
@@ -1,6 +1,15 @@
 package com.example.gomin.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.gomin.adapter.RecyclerViewItem
 
 class RankViewModel: ViewModel() {
+
+    private val _schoolRank = MutableLiveData<List<RecyclerViewItem>>()
+    val schoolRank: LiveData<List<RecyclerViewItem>> = _schoolRank
+
+    private val _studentRank = MutableLiveData<List<RecyclerViewItem>>()
+    val studentRank: LiveData<List<RecyclerViewItem>> = _studentRank
 }
