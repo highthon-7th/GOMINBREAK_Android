25e43801716ed4528adc484e07b7d324f1330736
Jaewon <turitus@naver.com>
2022-01-16 00:05:18 +0900

    [UPDATE] mypage repository

--- GOMIN_ANDROID/app/src/main/java/com/example/gomin/repository/MyPageRepository.kt
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/repository/MyPageRepository.kt
@@ -1,5 +1,15 @@
 package com.example.gomin.repository
 
-class MyPageRepository {
+import com.example.gomin.model.MyInfoEntity
+import com.example.gomin.remote.api.MyPageApi
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import retrofit2.Response
+
+class MyPageRepository(private val myPageApi: MyPageApi) {
     
+    fun getMyPageInfo(): Single<Response<MyInfoEntity>> =
+        myPageApi.getUserInfo().subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
 }
