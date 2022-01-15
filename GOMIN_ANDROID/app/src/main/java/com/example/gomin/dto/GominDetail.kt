2bac07f4e3b844774abba08475f8e32851d449d8
dlswer23 <lbj4298@naver.com>
2022-01-15 20:57:25 +0900

    [REFACTOR] gomin detail response

--- GOMIN_ANDROID/app/src/main/java/com/example/gomin/dto/GominDetail.kt
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/dto/GominDetail.kt
@@ -1,5 +1,3 @@
 package com.example.gomin.dto
 
-data class GominDetail(val gomin_title : String,val gomin : String,val gomin_user : String,val comments : List<UserComment>){
-
-}
+data class GominDetail(val gomin_title : String,val gomin : String,val gomin_user : String,val comments : List<UserComment>)
