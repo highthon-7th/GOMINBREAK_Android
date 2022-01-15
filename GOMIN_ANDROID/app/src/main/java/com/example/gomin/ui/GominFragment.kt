ccda11c50878540218170a084f519b0c173a2137
dlswer23 <lbj4298@naver.com>
2022-01-16 08:18:30 +0900

    [FIX] code

--- GOMIN_ANDROID/app/src/main/java/com/example/gomin/ui/GominFragment.kt
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/ui/GominFragment.kt
@@ -1,32 +1,36 @@
 package com.example.gomin.ui
 
+import androidx.lifecycle.ViewModel
 import com.example.gomin.R
 import com.example.gomin.base.BaseFragment
 import com.example.gomin.databinding.FragmentGominBinding
 import com.example.gomin.viewmodel.GominViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class GominFragment : BaseFragment<FragmentGominBinding>(R.layout.fragment_gomin) {
-
-    override val vm : GominViewModel by viewModel()
-
-    private val GominWriteDialog by lazy {
-        GominUploadDialog(vm)
-    }
-
-    private fun GominWriteDialog() {
-        GominWriteDialog.show(
-            requireActivity().supportFragmentManager,
-            "finishDialog"
-        )
-    }
-
+class GominFragment(override val vm: ViewModel) : BaseFragment<FragmentGominBinding>(R.layout.fragment_gomin) {
     override fun observeEvent() {
-        vm.run {
-            binding.floatingBtn.setOnClickListener {
-                GominWriteDialog()
-            }
-        }
-
+        TODO("Not yet implemented")
     }
+
+//    override val vm : GominViewModel by viewModel()
+//
+//    private val GominWriteDialog by lazy {
+//        GominUploadDialog(vm)
+//    }
+//
+//    private fun GominWriteDialog() {
+//        GominWriteDialog.show(
+//            requireActivity().supportFragmentManager,
+//            "finishDialog"
+//        )
+//    }
+//
+//    override fun observeEvent() {
+//        vm.run {
+//            binding.floatingBtn.setOnClickListener {
+//                GominWriteDialog()
+//            }
+//        }
+//
+//    }
 }
