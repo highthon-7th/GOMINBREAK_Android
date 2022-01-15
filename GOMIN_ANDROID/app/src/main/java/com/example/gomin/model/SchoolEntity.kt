737d9d0dda9207daf5d2b8a20a01e3488c660f0b
Jaewon <turitus@naver.com>
2022-01-15 16:59:05 +0900

    [UPDATE] search school

---
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/model/SchoolEntity.kt
@@ -0,0 +1,6 @@
+package com.example.gomin.model
+
+data class SchoolEntity(
+    val name: String,
+    val location: String
+)
--- GOMIN_ANDROID/app/src/main/java/com/example/gomin/repository/LoginRepository.kt
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/repository/LoginRepository.kt
@@ -1,5 +1,14 @@
 package com.example.gomin.repository
 
-class LoginRepository {
-    
+import com.example.gomin.model.SchoolEntity
+import com.example.gomin.remote.api.LoginApi
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import retrofit2.Response
+
+class LoginRepository(private val loginApi: LoginApi) {
+    fun searchSchool(name: String): Single<Response<List<SchoolEntity>>> =
+        loginApi.searchSchool(name).subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
 }
