4466d2b88a5a1011864f884d0b92f18ed9a8fcae
Jaewon <turitus@naver.com>
2022-01-15 23:58:11 +0900

    [UPDATE] save token

--- GOMIN_ANDROID/app/src/main/java/com/example/gomin/repository/LoginRepository.kt
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/repository/LoginRepository.kt
@@ -1,5 +1,6 @@
 package com.example.gomin.repository
 
+import com.example.gomin.local.LocalStorage
 import com.example.gomin.model.LoginEntity
 import com.example.gomin.model.RegisterEntity
 import com.example.gomin.model.SchoolEntity
@@ -9,9 +10,10 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.annotations.NonNull
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import retrofit2.Response
 
-class LoginRepository(private val loginApi: LoginApi) {
+class LoginRepository(private val loginApi: LoginApi, private val localStorage: LocalStorage) {
     fun searchSchool(name: String): @NonNull Single<Response<List<SchoolEntity>>> =
         loginApi.searchSchool(name).subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
@@ -20,7 +22,13 @@ class LoginRepository(private val loginApi: LoginApi) {
         loginApi.register(registerEntity).subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     fun login(loginEntity: LoginEntity): Single<Response<TokenEntity>> =
-        loginApi.login(loginEntity).subscribeOn(Schedulers.io())
+        loginApi.login(loginEntity).doOnSuccess {
+            if (it.body() != null) {
+                localStorage.saveAccessToken(it.body()!!.accessToken)
+                localStorage.saveRefreshToken(it.body()!!.refreshToken)
+            }
+        }.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
 }
