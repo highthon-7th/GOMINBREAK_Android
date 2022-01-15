fe589c87e39d8235aa6a99205f4e3a7d1a806189
Jaewon <turitus@naver.com>
2022-01-15 17:14:58 +0900

    [UPDATE] login api fun

--- GOMIN_ANDROID/app/src/main/java/com/example/gomin/remote/api/LoginApi.kt
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/remote/api/LoginApi.kt
@@ -1,12 +1,24 @@
 package com.example.gomin.remote.api
 
+import com.example.gomin.model.LoginEntity
+import com.example.gomin.model.RegisterEntity
 import com.example.gomin.model.SchoolEntity
+import com.example.gomin.model.TokenEntity
 import io.reactivex.rxjava3.core.Single
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface LoginApi {
+
     @GET("search/{name}")
     fun searchSchool(@Query("name") name: String): Single<Response<List<SchoolEntity>>>
+
+    @POST("register")
+    fun register(@Body request: RegisterEntity): Single<Response<Unit>>
+
+    @POST("login")
+    fun login(@Body request: LoginEntity): Single<Response<TokenEntity>>
 }
