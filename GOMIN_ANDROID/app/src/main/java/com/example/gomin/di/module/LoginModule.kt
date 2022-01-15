1808b557e415749520c20ba257ffef832f5d33e6
Jaewon <turitus@naver.com>
2022-01-15 17:31:09 +0900

    [UPDATE] login api

---
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/di/module/LoginModule.kt
@@ -0,0 +1,14 @@
+package com.example.gomin.di.module
+
+import com.example.gomin.remote.api.LoginApi
+import com.example.gomin.repository.LoginRepository
+import com.example.gomin.viewmodel.LoginViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import retrofit2.Retrofit
+
+val loginModule = module {
+    single<LoginApi> { get<Retrofit>().create(LoginApi::class.java) }
+    single { LoginRepository(get()) }
+    viewModel { LoginViewModel(get()) }
+}
--- GOMIN_ANDROID/app/src/main/java/com/example/gomin/viewmodel/LoginViewModel.kt
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/viewmodel/LoginViewModel.kt
@@ -1,8 +1,9 @@
 package com.example.gomin.viewmodel
 
+import androidx.lifecycle.ViewModel
 import com.example.gomin.repository.LoginRepository
 
-class LoginViewModel(private val loginRepository: LoginRepository) {
+class LoginViewModel(private val loginRepository: LoginRepository): ViewModel() {
     fun searchSchool(name: String){
         loginRepository.searchSchool(name).subscribe { result ->
 
