6dfd168363228237c6caca8e2e1d377493f14f10
Jaewon <turitus@naver.com>
2022-01-15 23:55:49 +0900

    [UPDATE] interceptor

--- GOMIN_ANDROID/app/src/main/java/com/example/gomin/local/LocalStorage.kt
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/local/LocalStorage.kt
@@ -1,14 +1,13 @@
 package com.example.gomin.local
 
-import android.content.Context
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.rxjava3.RxPreferenceDataStoreBuilder
 import androidx.datastore.rxjava3.RxDataStore
 import io.reactivex.rxjava3.core.Single
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
-class LocalStorage(private val context: Context) {
+class LocalStorage(private val dataStore: RxDataStore<Preferences>) {
+
     companion object {
         private val ACCESS_TOKEN_KEY = stringPreferencesKey("access_token")
         private val REFRESH_TOKEN_KEY = stringPreferencesKey("refresh_token")
@@ -16,7 +15,7 @@ class LocalStorage(private val context: Context) {
 
     @ExperimentalCoroutinesApi
     fun saveAccessToken(token: String) {
-        dataStore(context).updateDataAsync {
+        dataStore.updateDataAsync {
             val mutablePreference = it.toMutablePreferences()
             mutablePreference[ACCESS_TOKEN_KEY] = token
             Single.just(mutablePreference)
@@ -25,7 +24,7 @@ class LocalStorage(private val context: Context) {
 
     @ExperimentalCoroutinesApi
     fun saveRefreshToken(token: String) {
-        dataStore(context).updateDataAsync {
+        dataStore.updateDataAsync {
             val mutablePreference = it.toMutablePreferences()
             mutablePreference[REFRESH_TOKEN_KEY] = token
             Single.just(mutablePreference)
@@ -34,19 +33,15 @@ class LocalStorage(private val context: Context) {
 
     @ExperimentalCoroutinesApi
     fun getAccessToken(): String {
-        return dataStore(context).data().map {
+        return dataStore.data().map {
             "Bearer ${it[ACCESS_TOKEN_KEY] ?: ""}"
         }.blockingFirst()
     }
 
     @ExperimentalCoroutinesApi
     fun getRefreshToken(): String {
-        return dataStore(context).data().map {
+        return dataStore.data().map {
             it[REFRESH_TOKEN_KEY] ?: ""
         }.blockingFirst()
-
     }
 }
-
-fun dataStore(context: Context): RxDataStore<Preferences> =
-    RxPreferenceDataStoreBuilder(context, "gomin").build()
