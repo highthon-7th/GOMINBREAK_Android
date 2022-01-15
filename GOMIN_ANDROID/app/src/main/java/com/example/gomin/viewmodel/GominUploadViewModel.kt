41935d32fbe3592eb8d27a8f9bf7e5d41a5ff8cd
dlswer23 <lbj4298@naver.com>
2022-01-15 20:57:47 +0900

    [UPDATE] gomin viewmodel

--- GOMIN_ANDROID/app/src/main/java/com/example/gomin/viewmodel/GominUploadViewModel.kt
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/viewmodel/GominUploadViewModel.kt
@@ -1,4 +1,22 @@
 package com.example.gomin.viewmodel
 
-class GominUploadViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.gomin.base.SingleLiveEvent
+import com.example.gomin.repository.GominRepository
+
+class GominUploadViewModel(private val gominRepository: GominRepository) : ViewModel() {
+
+    val title = MutableLiveData<String>()
+    val content = MutableLiveData<String>()
+
+    val successUpload = SingleLiveEvent<Unit>()
+
+    fun upload() {
+        gominRepository.gominUpload(title.value!!, content.value!!).subscribe { response ->
+            if (response.isSuccessful){
+
+            }
+        }
+    }
 }
--- GOMIN_ANDROID/app/src/main/java/com/example/gomin/viewmodel/GominViewModel.kt
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/viewmodel/GominViewModel.kt
@@ -1,4 +1,37 @@
 package com.example.gomin.viewmodel
 
-class GominViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.gomin.dto.GominDetail
+import com.example.gomin.repository.GominRepository
+
+class GominViewModel(private val gominRepository: GominRepository) : ViewModel() {
+
+    private val gominId : String = ""
+
+    private val _gominDetailContent = MutableLiveData<GominDetail>()
+    val gominDetailContent :  LiveData<GominDetail> get() =  _gominDetailContent
+
+
+    private val _gominListObject = MutableLiveData<List<String>>()
+    val gominListObject :  LiveData<List<String>> get() = _gominListObject
+
+    fun getGominList() {
+        gominRepository.showGominList().subscribe { it ->
+            if (it.isSuccessful) {
+                _gominListObject.value = listOf(it.body().toString())
+            }
+        }
+    }
+
+    fun getGominDetail() {
+        gominRepository.showGominListDetail(gominId).subscribe { it ->
+            if (it.isSuccessful) {
+                _gominDetailContent.value = it.body()
+
+            }
+        }
+    }
+
 }
