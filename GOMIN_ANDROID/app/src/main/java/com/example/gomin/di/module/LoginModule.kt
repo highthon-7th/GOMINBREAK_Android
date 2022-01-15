94e89066ef4a1df5318444048ead52979e9aa6f5
Jaewon <turitus@naver.com>
2022-01-15 18:58:14 +0900

    [UPDATE] login module

--- GOMIN_ANDROID/app/src/main/java/com/example/gomin/di/module/LoginModule.kt
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/di/module/LoginModule.kt
@@ -3,6 +3,7 @@ package com.example.gomin.di.module
 import com.example.gomin.remote.api.LoginApi
 import com.example.gomin.repository.LoginRepository
 import com.example.gomin.viewmodel.LoginViewModel
+import com.example.gomin.viewmodel.RegisterViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -11,4 +12,5 @@ val loginModule = module {
     single<LoginApi> { get<Retrofit>().create(LoginApi::class.java) }
     single { LoginRepository(get()) }
     viewModel { LoginViewModel(get()) }
+    viewModel { RegisterViewModel(get()) }
 }
