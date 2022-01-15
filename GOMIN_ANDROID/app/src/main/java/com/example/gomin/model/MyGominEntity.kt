06a0adc080cf1794f7993f590d9096f432071a2b
Jaewon <turitus@naver.com>
2022-01-16 01:51:08 +0900

    [CREATE] my gomin entity

---
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/model/MyGominEntity.kt
@@ -0,0 +1,6 @@
+package com.example.gomin.model
+
+data class MyGominEntity(
+    val title: String,
+    val gomin: String
+)
--- GOMIN_ANDROID/app/src/main/java/com/example/gomin/ui/MyGominsActivity.kt
+++ GOMIN_ANDROID/app/src/main/java/com/example/gomin/ui/MyGominsActivity.kt
@@ -1,13 +1,15 @@
 package com.example.gomin.ui
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import com.example.gomin.R
 import com.example.gomin.base.BaseActivity
+import com.example.gomin.databinding.ActivityMyGominsBinding
+import com.example.gomin.viewmodel.MyGominsViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MyGominsActivity : BaseActivity<>() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_gomins)
+class MyGominsActivity : BaseActivity<ActivityMyGominsBinding>(R.layout.activity_my_gomins) {
+    override val vm: MyGominsViewModel by viewModel()
+
+    override fun observeEvent() {
+
     }
 }
